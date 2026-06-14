6be4af95de45c0d5d66a456f2330473d9f43c999
RoyK <rkokk@protonmail.com>
2026-06-14 21:19:39 +0200

    chore(app): improve debugging options

--- app.js
+++ app.js
@@ -6,9 +6,14 @@ const Homey = require('homey');
 class wallboxapp extends Homey.App {
 
 	onInit() {
-
-		if (process.env.DEBUG === '1')
-			inspector.open(8080, '0.0.0.0', true)
+		if (process.env.DEBUG === '1'){
+		try{
+			require('inspector').waitForDebugger();
+		}
+		catch(error){
+			require('inspector').open(9222, '0.0.0.0', true);
+		}
+		}
 
 		this.log('wallboxapp is running...');
 
